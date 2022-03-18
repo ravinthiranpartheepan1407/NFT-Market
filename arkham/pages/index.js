bcbaba92ecaa6fec83ddbffc7e79f7f9b7941736
ravinthiran-klvrs <ravinthiran@kleoverse.com>
2022-03-18 15:16:29 +0200

    Init

--- arkham/pages/index.js
+++ arkham/pages/index.js
@@ -4,15 +4,13 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div class="bg-background flex items-center justify-center h-screen bg-gray-200">
-  <div class="container">
-    <div class="bg-black rounded-lg shadow-lg p-5 md:p-20 mx-72">
-      <div class="text-center">
-        <h2 class="text-5xl font-Ubuntu font-medium tracking-tight leading-10 font-extrabold text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
-          ARKHAMM
-        </h2>
-        <h3 class='text-xl font-Ubuntu md:text-3xl mt-10'>Coming Soon</h3>
-        <p class="text-md font-Ubuntu md:text-xl mt-10"><a class="hover:underline" href="https://www.arkhamm.com">Arkhamm</a> is a blockchain platform to help the students to learn and get a job in Web3 technology </p>
+    <div className="bg-background flex items-center justify-center h-screen bg-gray-200">
+  <div className="container">
+    <div className="bg-black rounded-lg shadow-lg p-5 md:p-20 mx-72">
+      <div className="text-center">
+        
+        <h3 className='text-xl font-serif md:text-3xl mt-10'>Coming Soon</h3>
+        <p className="text-md font-serif md:text-xl mt-10"><a className="hover:underline" href="https://www.arkhamm.com">Arkhamm</a> is a blockchain platform to help the students to learn and get a job in Web3 technology </p>
       </div>
     </div>
   </div>
---
+++ pages/index.js
@@ -0,0 +1,21 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+export default function Home() {
+  return (
+    <div class="bg-background flex items-center justify-center h-screen bg-gray-200">
+  <div class="container">
+    <div class="bg-black rounded-lg shadow-lg p-5 md:p-20 mx-72">
+      <div class="text-center">
+        <h2 class="text-5xl font-serif font-medium tracking-tight leading-10 text-gray-900 sm:text-5xl sm:leading-none md:text-6xl">
+          ARKHAMM
+        </h2>
+        <h3 class='text-xl font-serif md:text-3xl mt-10'>Coming Soon</h3>
+        <p class="text-md font-serif md:text-xl mt-10"><a class="hover:underline" href="https://www.arkhamm.com">Arkhamm</a> is a blockchain platform to help the students to learn and get a job in Web3 technology </p>
+      </div>
+    </div>
+  </div>
+</div>
+  )
+}
