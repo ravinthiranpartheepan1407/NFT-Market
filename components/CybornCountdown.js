3ce8e08011b7e6111ce9b60c1bb0cef4481ce343
ravinthiran-klvrs <ravinthiran@kleoverse.com>
2022-03-18 20:24:42 +0200

    Landing

--- components/CybornCountdown.js
+++ components/CybornCountdown.js
@@ -77,7 +77,7 @@ class CybornCountdown extends Component {
         </div>
         <br />
         <br />
-        <button type="button" className="w-72 h-16 ml-20 text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 shadow-lg shadow-lime-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Explore</button>
+        <button type="button" className="w-72 h-16 ml-20 text-gray-900 bg-gradient-to-r from-lime-200 via-lime-400 to-lime-500 shadow-lg shadow-lime-500/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Coming Soon</button>
     </div>
 
 </div>
