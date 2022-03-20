7e2f7da183d1ef4db13f2c7dea45fbbdc485a114
ravinthiran-klvrs <ravinthiran@kleoverse.com>
2022-03-20 17:21:26 +0200

    NFT

--- pages/nft/inventory.js
+++ pages/nft/inventory.js
@@ -1,5 +1,5 @@
 import { ethers } from 'ethers'
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react"
 import axios from 'axios'
 import Web3Modal from "web3modal"
 import Link from 'next/link'
