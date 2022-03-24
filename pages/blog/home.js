d8f39eeb6245c566835939ec4d068589c7c4fcc2
ravinthiran-klvrs <ravinthiran@kleoverse.com>
2022-03-24 18:28:40 +0200

    Launch

--- pages/blog/home.js
+++ pages/blog/home.js
@@ -1,9 +1,17 @@
 import React from "react";
 import CybornHeader from "/components/CybornHeader";
 import CybornFooter from "/components/CybornFooter";
+import Head from "next/head";
 function home(){
   return(
     <div>
+    <Head>
+      <title>Arkhamm Web3</title>
+      <meta name="description" content="Arkhamm Blockchain" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/ark.png" />
+      <link rel="icon" type="image/png" sizes="32x32" href="/ark.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/ark.png" />
+    </Head>
       <CybornHeader />
       <br />
       <br />
