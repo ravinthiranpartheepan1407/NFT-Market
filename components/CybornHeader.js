3c5f0bbd5cf50a40b4596315a72245d5b6a2d403
ravinthiran-klvrs <ravinthiran@kleoverse.com>
2022-03-25 10:22:44 +0200

    Launch

--- components/CybornHeader.js
+++ components/CybornHeader.js
@@ -115,7 +115,7 @@ function CybornHeader(){
            </Link>
            <Link href="/ico/home">
              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-background hover:text-white">
-               ICO
+               Arkhamm Tokens
              </a>
            </Link>
            <Link href="/defi/home">
