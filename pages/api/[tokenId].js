cfdd2697b196ac1f36305f98a0e58fe5305e7747
Cyborn-NFT <general@cybornnft.com>
2022-05-31 17:52:04 +0300

    Game Tag

--- pages/api/[tokenId].js
+++ pages/api/[tokenId].js
@@ -1,15 +1,5 @@
 export default function handler(req, res) {
 
-  const arkhammGameTokenId = req.query.tokenId;
-
-
-  res.status(200).json({
-    name: "Arkhamm Game Tag #" + arkhammGameTokenId,
-    description: "Arkhamm Game NFT",
-    image: "https://gateway.pinata.cloud/ipfs/QmYgczxkjXQ1hheP9c5XcQ3tmQG1JZsZVLZPjSv4542xa1",
-  });
-}
-
   const arkhammTokenId = req.query.tokenId;
 
 
