d1a2fdb00dd9f6b18f1b95e9373b1cf245344634
Cyborn-NFT <general@cybornnft.com>
2022-05-31 18:59:44 +0300

    Game Tag

--- components/GameHead.js
+++ components/GameHead.js
@@ -15,6 +15,7 @@ import { ToastContainer } from 'react-toastify';
 
 
 export default function GameHead() {
+
   const [color, setColor] = useState("#20272c");
   const [textColor, setTextColor] = useState("#cacccf");
   const notify = (message) => toast(message, {
@@ -77,6 +78,12 @@ export default function GameHead() {
         </div>
       </div>
     );
+  };
+
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
   };
 
   const [walletConnected, setWalletConnected] = useState(false);
@@ -391,6 +398,30 @@ export default function GameHead() {
           <img src="/ark.png" width={100} height={100} className="rounded" />
          </a>
        </Link>
+       <button
+         className=' inline-flex p-3 hover:bg-background rounded lg:hidden text-white ml-auto hover:text-white outline-none'
+         onClick={handleClick}
+       >
+         <svg
+           className='w-6 h-6'
+           fill='none'
+           stroke='currentColor'
+           viewBox='0 0 24 24'
+           xmlns='http://www.w3.org/2000/svg'
+         >
+           <path
+             strokeLinecap='round'
+             strokeLinejoin='round'
+             strokeWidth={2}
+             d='M4 6h16M4 12h16M4 18h16'
+           />
+         </svg>
+       </button>
+       <div
+         className={`${
+           active ? '' : 'hidden'
+         }   w-full lg:inline-flex lg:flex-grow lg:w-auto`}
+       >
         <div className="lg:text-base lg:inline-flex md:space-x-0 md:mt-0 md:text-sm lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
            <Link href="/nft/market">
              <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-background hover:text-white ">
@@ -432,6 +463,7 @@ export default function GameHead() {
                Updates
              </a>
            </Link>
+          </div>
          <div>{renderButton()}</div>
        </div>
      </nav>
