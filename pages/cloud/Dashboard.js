ec6095e99e238dc72b488dcdf4e788e8de23259e
Cyborn-NFT <general@cybornnft.com>
2022-05-31 13:01:03 +0300

    Cloud Sync

--- pages/cloud/Dashboard.js
+++ pages/cloud/Dashboard.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 import {create as ipfsHttpClient} from "ipfs-http-client";
 import Web3Modal from "web3modal";
 import { ethers } from 'ethers'
+Import {Image} from "next/image"
 
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
@@ -167,12 +168,11 @@ function Dashboard(){
                   <div className="flex items-center justify-center w-full">
                       <label className="flex flex-col w-full h-32">
                           <div className="flex flex-col items-center justify-center pt-7">
-                              <svg xmlns="http://www.w3.org/2000/svg" className="w-12 h-12 text-white group-hover:text-white-600" viewBox="0 0 20 20" fill="currentColor">
-                                  <path fill-rule="evenodd" d="M4 3a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V5a2 2 0 00-2-2H4zm12 12H4l4-8 3 6 2-4 3 6z" clip-rule="evenodd" />
-                              </svg>
+                              <Image src="/gallery.png" className="w-12 h-12 text-white group-hover:text-white-600">
+                              </Image>
                           </div>
 
-                          <input type="file" className="opacity-0" onChange={onChange} type="file" />
+                          <input type="file" className="opacity-0" onChange={onChange} />
                       </label>
 
                   </div>
