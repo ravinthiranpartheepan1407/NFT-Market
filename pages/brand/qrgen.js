81fe7b42052a8e344979e73a9f826f461383cbce
Cyborn-NFT <general@cybornnft.com>
2022-06-01 20:29:28 +0300

    QR Syncd

--- pages/brand/qrgen.js
+++ pages/brand/qrgen.js
@@ -50,7 +50,7 @@ function QRgen() {
           Generate Ark QR for your Brand or Product Verification in Arkhamm on-chain Market
         </h1>
         <div style={{marginTop:30}}>
-            <input onChange={handleChange} placeholder="Enter Your Product / Brand URL" style={{width:520}} className="bg-background border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            <input onChange={handleChange} placeholder="Enter Your Product / Brand URL" style={{width:420}} className="bg-background border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             value={qr} label="QR content" size="large" variant="outlined" color="primary"
             />
         </div>
