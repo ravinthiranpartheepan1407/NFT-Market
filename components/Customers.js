6c1cb9a6b7df7441d483483f7ef0376f8934de4f
Cyborn-NFT <general@cybornnft.com>
2022-06-01 04:26:19 +0300

    Map fix

--- components/Customers.js
+++ components/Customers.js
@@ -9,7 +9,7 @@ function Customers(){
                     <img src="https://i.ibb.co/KjrPCyW/map.png" alt="world map image" className="w-full xl:h-full h-96 object-cover object-fill sm:block hidden" />
                     <img src="https://i.ibb.co/SXKj9Mf/map-bg.png" alt="mobile-image" className="sm:hidden -mt-10 block w-full h-96 object-cover object-fill absolute z-0" />
                     <div className="shadow-lg xl:p-6 p-4 sm:w-auto w-full bg-white sm:absolute relative z-20 sm:mt-0 mt-4 left-0 xl:ml-56 sm:ml-12 xl:-mt-40 sm:-mt-12">
-                        <p className="text-3xl font-semibold text-gray-800">125+</p>
+                        <p className="text-3xl font-semibold text-gray-800">10+</p>
                         <p className="text-base leading-4 xl:mt-4 mt-2 text-gray-600">Audience In USA</p>
                     </div>
 
