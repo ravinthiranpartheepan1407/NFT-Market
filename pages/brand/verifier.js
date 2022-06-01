4edca52d73076a3b35ffbf43c94beb78e209cbe7
Cyborn-NFT <general@cybornnft.com>
2022-06-01 15:51:40 +0300

    Flow QR Sync

--- pages/brand/verifier.js
+++ pages/brand/verifier.js
@@ -121,7 +121,6 @@ function Verifier(){
 
               />
               <br />
-              <br />
               <div className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">Upload QR*</div>
               <input name="Asset" required className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={onChange} type="file" />
               <br />
