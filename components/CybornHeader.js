9bd0cee36d3d35c9a6ba5b53a6a66283316038bd
Cyborn-NFT <general@cybornnft.com>
2022-06-02 19:32:16 +0300

    API Sync

--- components/CybornHeader.js
+++ components/CybornHeader.js
@@ -153,11 +153,6 @@ function CybornHeader(){
                Quant-Block-Encryption
              </a>
            </Link>
-           <Link href="/blog/home">
-             <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-background hover:text-white">
-               Updates
-             </a>
-           </Link>
            <p>
              <p className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:text-white">
                Account: {userAddress}
--- components/GameHead.js
+++ components/GameHead.js
@@ -481,11 +481,6 @@ export default function GameHead() {
                Quant-Block-Encryption
              </a>
            </Link>
-           <Link href="/blog/home">
-             <a className="lg:inline-flex lg:w-auto w-full px-3 py-2 rounded text-white items-center justify-center hover:bg-background hover:text-white">
-               Updates
-             </a>
-           </Link>
           </div>
          <div>{renderButton()}</div>
        </div>
